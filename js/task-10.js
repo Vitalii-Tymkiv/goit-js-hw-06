2cea292c4d58ab2adf27060cc09b6205c212c10a
Vitalii Tymkiv <vitaliy.tymkiv@gmail.com>
2022-09-13 12:42:05 +0300

    Update & finished task 10

--- js/task-10.js
+++ js/task-10.js
@@ -10,28 +10,19 @@ const inputAmountRef = document.querySelector("#controls input");
 creatBtnRef.addEventListener("click", createBoxes);
 destroyBtnRef.addEventListener("click", destroyBoxes);
 
-// inputAmountRef.addEventListener("input", (elem) => {
-//   console.log(elem.currentTarget.value);
-//   elem.currentTarget.value;
-// });
-
-const markup = [];
-
-function createBoxes(amount) {
+function createBoxes() {
+  const markup = [];
   for (let i = 0; i <= inputAmountRef.value; i += 1) {
     const newDivEl = `<div style ="background-color: ${getRandomHexColor()};width: ${
       30 + i * 10
     }px; height: ${30 + i * 10}px"></div>`;
-    markup.push(newDivEl);
 
+    markup.push(newDivEl);
     console.log(markup);
-    return markup;
   }
+  boxesContainerRef.insertAdjacentHTML("beforeend", markup.join(""));
 }
 
-boxesContainerRef.insertAdjacentHTML("beforeend", markup.join(""));
-
-// console.log(boxesContainerRef);
 function destroyBoxes() {
   boxesContainerRef.innerHTML = "";
 }
